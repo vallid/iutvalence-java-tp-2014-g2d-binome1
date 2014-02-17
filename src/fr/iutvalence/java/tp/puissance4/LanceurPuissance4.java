75b455c9f9d58accc7992208b7c8f031ea1cd7e7
vallid <dylan.valli@iut-valence.fr>
2014-02-17 11:02:51 +0100

    création class LanceurPuissance4
    création du squelette de la class LanceurPuissance4 qui permet de lancer
    la partie

---
+++ src/fr/iutvalence/java/tp/puissance4/LanceurPuissance4.java
@@ -0,0 +1,20 @@
+package fr.iutvalence.java.tp.puissance4;
+/**
+ * Permet de lancer une partie de Puissance4
+ * @author vallid
+ * 
+ */
+public class LanceurPuissance4
+{ 
+	/**
+	 * Creation et démarrage d'une partie de Puissance 4
+	 * @param args les arguments de la ligne de commande (aucun pour l'instant)
+	 */
+	public static void main (String[]args)
+	{
+		Puissance4 jeu; 
+		jeu = new Puissance4();
+		
+		jeu.jouer();
+	}
+}
--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -1,3 +1,4 @@
+package fr.iutvalence.java.tp.puissance4;
 /**
  * @author vallid
  *creation d'une partie de puissance 4
@@ -5,4 +6,10 @@
 public class Puissance4
 {
 
+	public void jouer()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
