77a673b64a5c1a58c95ea1158acccdd76f955231
vallid <dylan.valli@iut-valence.fr>
2014-04-03 09:50:13 +0200

    Modification de méthodes sur la classe Puissance4
    Renommage de l'attribut "grille" en "plateau"
    Suppression de la méthode obtenirGrille
    Modification de la méthode poserJeton

--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -2,8 +2,9 @@ package fr.iutvalence.java.tp.puissance4;
 
 /**
  * définition d'une partie de puissance 4
+ * 
  * @author vallid
- *
+ * 
  */
 public class Puissance4
 {
@@ -16,53 +17,49 @@ public class Puissance4
 	 * joueur 2
 	 */
 	private final Joueur joueur2;
-	
+
 	/**
-	 * plateau du jeu 
+	 * plateau du jeu
 	 */
-	private final Grille plateau;
-	
+	private final Grille grille;
+
 	/**
 	 * Permet de créer une nouvelle partie de puissance4 sur une grille de 7*6
 	 * 
 	 */
 	public Puissance4()
 	{
-		this.joueur1= new Joueur();
-		this.joueur2= new Joueur();
-		this.plateau= new Grille();	
+		this.joueur1 = new Joueur();
+		this.joueur2 = new Joueur();
+		this.grille = new Grille();
 	}
-	
+
 	/**
 	 * lance la partie de Puissance4
 	 */
 	public void jouer()
 	{
 		System.out.println("la partie va commencer");
+		System.out.println(this.grille);
 	}
-	
+
 	/**
-	 * Permet d'obtenir la grille du jeu
-	 * @return grille du jeu
-	 */
-	public Grille obtenirGrille()
-	{
-		return this.plateau;
-	}
-	
-	 /**
 	 * Permet de poser un jeton dans une colonne de la grille.
-	 * @param couleur Couleur du jeton
-	 * @param colonne Colonne où poser le jeton
+	 * 
+	 * @param couleur
+	 *            Couleur du jeton
+	 * @param numeroDeColonne
+	 *            Colonne où poser le jeton
 	 */
-	private void poserJeton(EtatsCase couleur,int colonne)
+	private boolean poserJeton(EtatCase couleur, int numeroDeColonne)
 	{
-		int ligne=0;
-		while(this.plateau.obtenirCase(colonne, ligne).obtenirEtat()!=EtatsCase.NONOCCUPE);
-		{
-			ligne++;
-		}
-		this.plateau.obtenirCase(colonne, ligne).changerEtatCase(couleur);
+		int numeroDeLigne;
+		for (numeroDeLigne=0;numeroDeLigne<Grille.NOMBRE_DE_LIGNES; numeroDeLigne++)
+			if (this.grille.obtenirCase(numeroDeLigne, numeroDeColonne) == EtatCase.VIDE) break;
+			numeroDeLigne++;
+		if (numeroDeLigne == Grille.NOMBRE_DE_LIGNES) return false;
+		this.grille.changerEtatCase(numeroDeLigne, numeroDeColonne, couleur);
+		return true;
 	}
-	
+
 }
