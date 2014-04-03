7b5cd75587e8f3d8307c7f30ecec325064d957ac
vallid <dylan.valli@iut-valence.fr>
2014-04-03 10:05:49 +0200

    Modification de la classe Grille
    Redéfinition des constantes
    Le plateau est maintenant défini par un tableau d'EtatCase
    Suppression du constructeur permettant de choisir les dimensions du
    puissance 4
    Renommage d'afficherPlateau en toString

--- src/fr/iutvalence/java/tp/puissance4/Grille.java
+++ src/fr/iutvalence/java/tp/puissance4/Grille.java
@@ -3,6 +3,7 @@ package fr.iutvalence.java.tp.puissance4;
 // TODO(Fait) corriger le commentaire
 /**
  * Représente une grille de Puissance4
+ * 
  * @author vallid
  */
 public class Grille
@@ -10,92 +11,78 @@ public class Grille
 	/**
 	 * la longueur par defaut de la grille
 	 */
-	public static final int LONGUEUR_GRILLE_DEFAUT = 7;
-	
+	public static final int NOMBRE_DE_LIGNES = 6;
+
 	/**
 	 * la largeur par defaut de la grille
 	 */
-	public static final int LARGEUR_GRILLE_DEFAUT = 6;
+	public static final int NOMBRE_DE_COLONNES = 7;
 
 	/**
-	 * tableau de cases pour définir la grille 
+	 * tableau de cases pour définir la grille
 	 */
-	public Case[][] cases;
+	public EtatCase[][] etatsDesCases;
 
-	// TODO(fait) écrire un commentaire plus précis (comment est initialisée la grille ?)
+	// TODO(fait) écrire un commentaire plus précis (comment est initialisée la
+	// grille ?)
 	/**
 	 * Permet de créer une grille de taille par defaut : 7 colonnes et 6 lignes
 	 * Les cases de la grille sont non occupées
 	 */
 	public Grille()
 	{
-		 this.cases = new Case[LONGUEUR_GRILLE_DEFAUT][LARGEUR_GRILLE_DEFAUT];
-			
-		for(int ligne=0;ligne<LARGEUR_GRILLE_DEFAUT;ligne++)
-		{
-			for(int colonne=0;colonne<LONGUEUR_GRILLE_DEFAUT;colonne++)
-			{
-				this.cases[colonne][ligne]= new Case();
-			}
-		}
+		this.etatsDesCases = new EtatCase[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
+
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+				this.etatsDesCases[numeroDeLigne][numeroDeColonne] = EtatCase.VIDE;
 	}
-	
-	// TODO (fait)écrire un commentaire plus précis (comment est initialisée la grille ?)
+
 	/**
-	 * Permet de créer une nouvelle grille avec une longueur et une largeur choisie
-	 * Les cases de la grille sont non occupées
+	 * Permet d'obtenir une case à la colonne et à la ligne indiqués en
+	 * paramètre
 	 * 
-	 * @param longueur longueur choisie
-	 * @param largeur largeur choisie 
-	 */
-	
-	public Grille (int longueur,int largeur)
-	{
-		this.cases = new Case [longueur][largeur];
-		
-		for(int ligne=0;ligne<largeur;ligne++)
-		{
-			for(int colonne=0;colonne<longueur;colonne++)
-			{
-				this.cases[colonne][ligne]= new Case();
-			}
-		}
-	}
-	
-	/**
-	 * Permet d'obtenir une case à la colonne et à la ligne indiqués en paramètre
-	 * @param colonne : Colonne de la case à obtenir
-	 * @param ligne : Ligne de la case à Obtenir
+	 * @param numeroDeLigne
+	 *            : Colonne de la case à obtenir
+	 * @param numeroDeColonne
+	 *            : Ligne de la case à Obtenir
 	 * @return La case
 	 */
-	public Case obtenirCase(int colonne, int ligne)
+	public EtatCase obtenirCase(int numeroDeLigne, int numeroDeColonne)
 	{
-		return this.cases[colonne][ligne];
+		// TODO(fait) cas d'erreur ? soit return null, soit une exception
+		if (numeroDeLigne<NOMBRE_DE_LIGNES && numeroDeColonne<NOMBRE_DE_COLONNES)
+		return this.etatsDesCases[numeroDeLigne][numeroDeColonne];
+		else return null;
 	}
 	
+	/**
+	 * Permet de changer l'état d'une case du plateau
+	 * @param numeroDeLigne numero de la ligne où se situe la case
+	 * @param numeroDeColonne numero de la colonne où se situe la case
+	 * @param nouvelEtat etat de la case après la modification
+	 */
+	public void changerEtatCase(int numeroDeLigne, int numeroDeColonne, EtatCase nouvelEtat)
+	{
+		// TODO (Fait) à completer
+		this.etatsDesCases[numeroDeLigne][numeroDeColonne]=nouvelEtat;
+	}
 	/**
 	 * Permet de représenter le puissance 4 en ASCII ART
 	 * 
 	 * @return Représentation ASCII du Puissance 4
 	 */
-	
-	public String afficherPlateau()
+
+	public String toString()
 	{
 		String puissance4EnAscii = "";
-		
-		for (int ligne=0;ligne<LARGEUR_GRILLE_DEFAUT;ligne++)
+
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
 		{
-			for (int colonne=0;colonne<LONGUEUR_GRILLE_DEFAUT;colonne++)
-			{
-				puissance4EnAscii += this.obtenirCase(colonne, ligne).obtenirEtat().toString();
-				
-				if (colonne == 6)
-				{
-					puissance4EnAscii += "\n";
-				}
-			}
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+				puissance4EnAscii += this.obtenirCase(numeroDeLigne, numeroDeColonne);
+			puissance4EnAscii += "\n";
 		}
 		return puissance4EnAscii;
 	}
 }
-	
