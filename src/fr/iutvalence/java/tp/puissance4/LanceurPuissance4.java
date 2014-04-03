ea60060a34446077f9a64c407e9e1cb588fb2092
vallid <dylan.valli@iut-valence.fr>
2014-04-03 09:48:15 +0200

    Modification du main
    L'affichage du plateau est maintenant dans la méthode jouer

--- src/fr/iutvalence/java/tp/puissance4/LanceurPuissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/LanceurPuissance4.java
@@ -1,20 +1,23 @@
 package fr.iutvalence.java.tp.puissance4;
+
 /**
  * Permet de lancer une partie de Puissance4
+ * 
  * @author vallid
  * 
  */
 public class LanceurPuissance4
-{ 
+{
 	/**
 	 * Creation et démarrage d'une partie de Puissance 4
-	 * @param args les arguments de la ligne de commande (aucun pour l'instant)
+	 * 
+	 * @param args
+	 *            les arguments de la ligne de commande (aucun pour l'instant)
 	 */
-	public static void main (String[]args)
+	public static void main(String[] args)
 	{
-		Puissance4 jeu; 
+		Puissance4 jeu;
 		jeu = new Puissance4();
 		jeu.jouer();
-		jeu.obtenirGrille().afficherPlateau();
 	}
 }
