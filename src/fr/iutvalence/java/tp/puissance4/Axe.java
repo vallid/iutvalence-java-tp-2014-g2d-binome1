574b93e4d237a7d38becd7243725e9889ed0d42e
vallid <dylan.valli@iut-valence.fr>
2014-04-16 17:40:53 +0200

    Ecriture de la Javadoc sur Axe

--- src/fr/iutvalence/java/tp/puissance4/Axe.java
+++ src/fr/iutvalence/java/tp/puissance4/Axe.java
@@ -1,26 +1,62 @@
 package fr.iutvalence.java.tp.puissance4;
 
+/**
+ * Liste des Axes du puissance4
+ * @author vallid
+ *
+ */
 public enum Axe
 {
+	/**
+	 * Ligne du puissance4
+	 */
 	LIGNE(Direction.GAUCHE, Direction.DROITE),
+	/**
+	 * Colonne du puissance4
+	 */
 	COLONNE(Direction.HAUT, Direction.BAS),
+	/**
+	 * Diagonale du puissance 4
+	 */
 	DIAGONALE1(Direction.HAUT_GAUCHE, Direction.BAS_DROITE),
+	/**
+	 * Autre diagonale du puissance 4
+	 */
 	DIAGONALE2(Direction.HAUT_DROITE, Direction.BAS_GAUCHE);
 	
+	/**
+	 * Direction principale de l'axe
+	 */
 	private final Direction principale;
+	/**
+	 * Direction opposée à la principale
+	 */
 	private final Direction secondaire;
 	
+	/**
+	 * Permet de créer un nouvel Axe
+	 * @param principale Direction principale de l'Axe
+	 * @param secondaire Direction opposée
+	 */
 	private Axe(Direction principale, Direction secondaire)
 	{
 		this.principale=principale;
 		this.secondaire=secondaire;
 	}
 
+	/**
+	 * Permet d'obtenir la direction principale de l'axe
+	 * @return Direction principale
+	 */
 	public Direction obtenirPrincipale()
 	{
 		return this.principale;
 	}
 	
+	/**
+	 * Permet d'obtenir la direction secondaire de l'axe
+	 * @return Direction secondaire
+	 */
 	public Direction obtenirSecondaire()
 	{
 		return this.secondaire;
