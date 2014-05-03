2e083692ca753fc6ed34ab1ca3c0a85cc8693c9c
sebastienjean <baz.jean@gmail.com>
2014-05-03 09:42:59 +0200

    Nettoyage de la classe Puissance4

--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -1,8 +1,9 @@
 package fr.iutvalence.java.tp.puissance4;
+
 import java.util.Scanner;
 
 /**
- * définition d'une partie de puissance 4
+ * Représente une partie de puissance 4
  * 
  * @author vallid
  * 
@@ -10,22 +11,22 @@ import java.util.Scanner;
 public class Puissance4
 {
 	/**
-	 * joueur 1
+	 * Le premier joueur
 	 */
 	private final Joueur joueur1;
 
 	/**
-	 * joueur 2
+	 * Le second joueur
 	 */
 	private final Joueur joueur2;
 
 	/**
-	 * plateau du jeu
+	 * Le plateau du jeu
 	 */
 	private final Grille grille;
 
 	/**
-	 * Permet de créer une nouvelle partie de puissance4 sur une grille de 7*6
+	 * Créer une nouvelle partie de puissance4 sur une grille de 7*6
 	 * 
 	 */
 	public Puissance4()
@@ -36,29 +37,34 @@ public class Puissance4
 	}
 
 	/**
-	 * lance la partie de Puissance4
+	 * Démarrer la partie de Puissance4
 	 */
 	public void jouer()
 	{
-		Scanner saisieUtilisateur = new Scanner(System.in); 
-		
-		System.out.println("la partie va commencer. Joueur 1: Pions rouges, Joueur 2: Pions jaunes.");
+		Scanner saisieUtilisateur = new Scanner(System.in);
+
+		System.out
+				.println("la partie va commencer. Joueur 1: Pions rouges, Joueur 2: Pions jaunes.");
 		System.out.println(this.grille);
-		
-		while(true)
+
+		while (true)
 		{
-			System.out.println("Joueur 1, entrez la colonne dans laquelle vous voulez poser un jeton");
+			System.out
+					.println("Joueur 1, entrez la colonne dans laquelle vous voulez poser un jeton");
 			int colonne = saisieUtilisateur.nextInt();
-			if (this.estGagnee(this.poserJeton(EtatCase.ROUGE, colonne)))
+			if (this.estPartieGagneeAutourDUnePosition(this.poserJeton(EtatCase.ROUGE,
+					colonne)))
 			{
 				System.out.println("Le joueur 1 gagne la partie !");
 				break;
 			}
 			System.out.println(this.grille);
-			
-			System.out.println("Joueur 2, entrez la colonne dans laquelle vous voulez poser un jeton");
+
+			System.out
+					.println("Joueur 2, entrez la colonne dans laquelle vous voulez poser un jeton");
 			colonne = saisieUtilisateur.nextInt();
-			if (this.estGagnee(this.poserJeton(EtatCase.JAUNE, colonne)))
+			if (this.estPartieGagneeAutourDUnePosition(this.poserJeton(EtatCase.JAUNE,
+					colonne)))
 			{
 				System.out.println("Le joueur 1 gagne la partie !");
 				break;
@@ -68,22 +74,23 @@ public class Puissance4
 	}
 
 	/**
-	 * Permet de poser un jeton dans une colonne de la grille.
+	 * Tenter de poser un jeton dans une colonne de la grille.
 	 * 
 	 * @param couleur
-	 *            Couleur du jeton
+	 *            la couleur du jeton à poser
 	 * @param numeroDeColonne
-	 *            Colonne où poser le jeton
-	 *
+	 *            le numéro de colonne où poser le jeton
+	 * 
+	 * @return la position où le jeton a été posée, ou null si la colonne était
+	 *         pleine.
+	 * 
 	 */
 	private Position poserJeton(EtatCase couleur, int numeroDeColonne)
 	{
-		
-		
-		for (int numeroDeLigne = this.grille.NOMBRE_DE_LIGNES-1; numeroDeLigne >=0; numeroDeLigne--)
+		for (int numeroDeLigne = this.grille.NOMBRE_DE_LIGNES - 1; numeroDeLigne >= 0; numeroDeLigne--)
 		{
 			Position position = new Position(numeroDeLigne, numeroDeColonne);
-			if (this.grille.obtenirCase(position)==EtatCase.VIDE)
+			if (this.grille.obtenirCase(position) == EtatCase.VIDE)
 			{
 				this.grille.changerEtatCase(position, couleur);
 				return position;
@@ -91,32 +98,46 @@ public class Puissance4
 		}
 		return null;
 	}
-		
+
 	/**
-	 * Permet de savoir si la partie est gagnée
-	 * @param position Position a tester
-	 * @return True si la partie est gagnée
+	 * Permet de savoir si la partie est gagnée, quel que soit l'axe, autour
+	 * d'une position
+	 * 
+	 * @param position
+	 *            la position
+	 * @return <tt>true</tt> si la partie est gagnée autour de la position
 	 */
-	private boolean estGagnee(Position position)
+	private boolean estPartieGagneeAutourDUnePosition(Position position)
 	{
 		for (Axe axe : Axe.values())
-			if (this.estGagneAxe(position, axe)) return true;
+			if (this.estPartieGagneeAutourDUnePositionEtDansUnAxe(position, axe))
+				return true;
 		return false;
 	}
-	
+
 	/**
-	 * Permet de savoir si la partie est gagnée selon un axe
-	 * @param position Position à tester
-	 * @param axe Axe a tester
-	 * @return True si la partie est gagnée
+	 * Permet de savoir si la partie est gagnée autour d'une position dans un
+	 * axe donné
+	 * 
+	 * @param position
+	 *            la position
+	 * @param axe
+	 *            l'axe
+	 * 
+	 * @return <tt>true</tt> si la partie est gagnée autour de la position dans l'axe
 	 */
-	public boolean estGagneAxe(Position position, Axe axe)
+	private boolean estPartieGagneeAutourDUnePositionEtDansUnAxe(Position position, Axe axe)
 	{
-		int nombreDeJetonsDansLaDirectionPrincipale = this.nombreDeJetonsAlignesDansUneDirection(position, axe.obtenirPrincipale());
-		int nombreDeJetonsDansLaDirectionSecondaire = this.nombreDeJetonsAlignesDansUneDirection(position, axe.obtenirSecondaire());
-		return (nombreDeJetonsDansLaDirectionPrincipale + nombreDeJetonsDansLaDirectionSecondaire + 1 >= 4);		
+		int nombreDeJetonsDansLaDirectionPrincipale = this
+				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
+						axe.obtenirPrincipale());
+		int nombreDeJetonsDansLaDirectionSecondaire = this
+				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
+						axe.obtenirSecondaire());
+		return (nombreDeJetonsDansLaDirectionPrincipale
+				+ nombreDeJetonsDansLaDirectionSecondaire + 1 >= 4);
 	}
-	
+
 	/**
 	 * @param position
 	 * @param direction
@@ -124,19 +145,24 @@ public class Puissance4
 	 */
 	/**
 	 * Permet de connaitre le nombre de jetons alignés sur une direction
-	 * @param position Position d'où l'on commence à compter
-	 * @param direction Direction où l'on compte
+	 * 
+	 * @param position
+	 *            Position d'où l'on commence à compter
+	 * @param direction
+	 *            Direction où l'on compte
 	 * @return Le nombre de jetons alignés
 	 */
-	private int nombreDeJetonsAlignesDansUneDirection(Position position, Direction direction)
+	private int obtenirNombreDeJetonsAlignesDansUneDirection(Position position,
+			Direction direction)
 	{
 		int nombreDeJetonsAlignes = 0;
 		Position positionSuivante = position.obtenirVoisine(direction);
 		while (true)
 		{
-			if (this.grille.estHorsDeLaGrille(positionSuivante)) 
+			if (this.grille.estHorsDeLaGrille(positionSuivante))
 				break;
-			if (this.grille.obtenirCase(positionSuivante) != this.grille.obtenirCase(position))
+			if (this.grille.obtenirCase(positionSuivante) != this.grille
+					.obtenirCase(position))
 				break;
 			nombreDeJetonsAlignes++;
 		}
