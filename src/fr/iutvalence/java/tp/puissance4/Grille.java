7f275d467ab6fb0796af34aa50ef2edfa48613f5
sebastienjean <baz.jean@gmail.com>
2014-05-03 09:49:59 +0200

    Déplacement de méthodes de Puissance4 dans Grille

--- src/fr/iutvalence/java/tp/puissance4/Grille.java
+++ src/fr/iutvalence/java/tp/puissance4/Grille.java
@@ -40,7 +40,7 @@ public class Grille
 	 * @param position la position
 	 * @return l'état de la case la position donnée
 	 */
-	public EtatCase obtenirCase(Position position)
+	private EtatCase obtenirCase(Position position)
 	{
 		if (position.obtenirNumeroDeLigne()<NOMBRE_DE_LIGNES && position.obtenirNumeroDeColonne()<NOMBRE_DE_COLONNES)
 		return this.etatsDesCases[position.obtenirNumeroDeLigne()][position.obtenirNumeroDeColonne()];
@@ -54,26 +54,10 @@ public class Grille
 	 * @param position la position
 	 * @param nouvelEtat le nouvel état de la case
 	 */
-	public void changerEtatCase(Position position, EtatCase nouvelEtat)
+	private void changerEtatCase(Position position, EtatCase nouvelEtat)
 	{
 		// TODO traiter le cas d'erreur (débordement de grille)
 		this.etatsDesCases[position.obtenirNumeroDeLigne()][position.obtenirNumeroDeColonne()]=nouvelEtat;
-	}
-
-	/**
-	 * Obtenir une représentation en ASCII-art
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString()
-	{
-		String puissance4EnAscii = "";
-		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-		{
-			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-				puissance4EnAscii += this.obtenirCase(new Position(numeroDeLigne, numeroDeColonne));
-			puissance4EnAscii += "\n";
-		}
-		return puissance4EnAscii;
 	}
 
 	/**
@@ -88,5 +72,112 @@ public class Grille
 			if (position.obtenirNumeroDeColonne()>=0 && position.obtenirNumeroDeColonne()< NOMBRE_DE_COLONNES)
 				return true;
 		return false;
+	}
+	
+	/**
+	 * Tenter de poser un jeton dans une colonne de la grille.
+	 * 
+	 * @param couleur
+	 *            la couleur du jeton à poser
+	 * @param numeroDeColonne
+	 *            le numéro de colonne où poser le jeton
+	 * 
+	 * @return la position où le jeton a été posée, ou null si la colonne était
+	 *         pleine.
+	 * 
+	 */
+	public Position poserJeton(EtatCase couleur, int numeroDeColonne)
+	{
+		for (int numeroDeLigne = Grille.NOMBRE_DE_LIGNES - 1; numeroDeLigne >= 0; numeroDeLigne--)
+		{
+			Position position = new Position(numeroDeLigne, numeroDeColonne);
+			if (this.obtenirCase(position) == EtatCase.VIDE)
+			{
+				this.changerEtatCase(position, couleur);
+				return position;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Permet de savoir si la partie est gagnée, quel que soit l'axe, autour
+	 * d'une position
+	 * 
+	 * @param position
+	 *            la position
+	 * @return <tt>true</tt> si la partie est gagnée autour de la position
+	 */
+	public boolean estPartieGagneeAutourDUnePosition(Position position)
+	{
+		for (Axe axe : Axe.values())
+			if (this.estPartieGagneeAutourDUnePositionEtDansUnAxe(position, axe))
+				return true;
+		return false;
+	}
+
+	/**
+	 * Permet de savoir si la partie est gagnée autour d'une position dans un
+	 * axe donné
+	 * 
+	 * @param position
+	 *            la position
+	 * @param axe
+	 *            l'axe
+	 * 
+	 * @return <tt>true</tt> si la partie est gagnée autour de la position dans l'axe
+	 */
+	private boolean estPartieGagneeAutourDUnePositionEtDansUnAxe(Position position, Axe axe)
+	{
+		int nombreDeJetonsDansLaDirectionPrincipale = this
+				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
+						axe.obtenirPrincipale());
+		int nombreDeJetonsDansLaDirectionSecondaire = this
+				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
+						axe.obtenirSecondaire());
+		return (nombreDeJetonsDansLaDirectionPrincipale
+				+ nombreDeJetonsDansLaDirectionSecondaire + 1 >= 4);
+	}
+	
+	/**
+	 * Permet de connaitre le nombre de jetons alignés sur une direction
+	 * 
+	 * @param position
+	 *            Position d'où l'on commence à compter
+	 * @param direction
+	 *            Direction où l'on compte
+	 * @return Le nombre de jetons alignés
+	 */
+	private int obtenirNombreDeJetonsAlignesDansUneDirection(Position position,
+			Direction direction)
+	{
+		int nombreDeJetonsAlignes = 0;
+		Position positionSuivante = position.obtenirVoisine(direction);
+		while (true)
+		{
+			if (this.estHorsDeLaGrille(positionSuivante))
+				break;
+			if (this.obtenirCase(positionSuivante) != this.obtenirCase(position))
+				break;
+			nombreDeJetonsAlignes++;
+		}
+		return nombreDeJetonsAlignes;
+	}
+
+	
+	/**
+	 * Obtenir une représentation en ASCII-art
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString()
+	{
+		String puissance4EnAscii = "";
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
+		{
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+				puissance4EnAscii += this.obtenirCase(new Position(numeroDeLigne, numeroDeColonne));
+			puissance4EnAscii += "\n";
+		}
+		return puissance4EnAscii;
 	}
 }
--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -40,7 +40,7 @@ public class Puissance4
 			System.out
 					.println("Joueur 1, entrez la colonne dans laquelle vous voulez poser un jeton");
 			int colonne = saisieUtilisateur.nextInt();
-			if (this.estPartieGagneeAutourDUnePosition(this.poserJeton(EtatCase.ROUGE,
+			if (this.grille.estPartieGagneeAutourDUnePosition(this.grille.poserJeton(EtatCase.ROUGE,
 					colonne)))
 			{
 				System.out.println("Le joueur 1 gagne la partie !");
@@ -51,7 +51,7 @@ public class Puissance4
 			System.out
 					.println("Joueur 2, entrez la colonne dans laquelle vous voulez poser un jeton");
 			colonne = saisieUtilisateur.nextInt();
-			if (this.estPartieGagneeAutourDUnePosition(this.poserJeton(EtatCase.JAUNE,
+			if (this.grille.estPartieGagneeAutourDUnePosition(this.grille.poserJeton(EtatCase.JAUNE,
 					colonne)))
 			{
 				System.out.println("Le joueur 1 gagne la partie !");
@@ -60,101 +60,4 @@ public class Puissance4
 			System.out.println(this.grille);
 		}
 	}
-
-	/**
-	 * Tenter de poser un jeton dans une colonne de la grille.
-	 * 
-	 * @param couleur
-	 *            la couleur du jeton à poser
-	 * @param numeroDeColonne
-	 *            le numéro de colonne où poser le jeton
-	 * 
-	 * @return la position où le jeton a été posée, ou null si la colonne était
-	 *         pleine.
-	 * 
-	 */
-	private Position poserJeton(EtatCase couleur, int numeroDeColonne)
-	{
-		for (int numeroDeLigne = Grille.NOMBRE_DE_LIGNES - 1; numeroDeLigne >= 0; numeroDeLigne--)
-		{
-			Position position = new Position(numeroDeLigne, numeroDeColonne);
-			if (this.grille.obtenirCase(position) == EtatCase.VIDE)
-			{
-				this.grille.changerEtatCase(position, couleur);
-				return position;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Permet de savoir si la partie est gagnée, quel que soit l'axe, autour
-	 * d'une position
-	 * 
-	 * @param position
-	 *            la position
-	 * @return <tt>true</tt> si la partie est gagnée autour de la position
-	 */
-	private boolean estPartieGagneeAutourDUnePosition(Position position)
-	{
-		for (Axe axe : Axe.values())
-			if (this.estPartieGagneeAutourDUnePositionEtDansUnAxe(position, axe))
-				return true;
-		return false;
-	}
-
-	/**
-	 * Permet de savoir si la partie est gagnée autour d'une position dans un
-	 * axe donné
-	 * 
-	 * @param position
-	 *            la position
-	 * @param axe
-	 *            l'axe
-	 * 
-	 * @return <tt>true</tt> si la partie est gagnée autour de la position dans l'axe
-	 */
-	private boolean estPartieGagneeAutourDUnePositionEtDansUnAxe(Position position, Axe axe)
-	{
-		int nombreDeJetonsDansLaDirectionPrincipale = this
-				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
-						axe.obtenirPrincipale());
-		int nombreDeJetonsDansLaDirectionSecondaire = this
-				.obtenirNombreDeJetonsAlignesDansUneDirection(position,
-						axe.obtenirSecondaire());
-		return (nombreDeJetonsDansLaDirectionPrincipale
-				+ nombreDeJetonsDansLaDirectionSecondaire + 1 >= 4);
-	}
-
-	/**
-	 * @param position
-	 * @param direction
-	 * @return
-	 */
-	/**
-	 * Permet de connaitre le nombre de jetons alignés sur une direction
-	 * 
-	 * @param position
-	 *            Position d'où l'on commence à compter
-	 * @param direction
-	 *            Direction où l'on compte
-	 * @return Le nombre de jetons alignés
-	 */
-	private int obtenirNombreDeJetonsAlignesDansUneDirection(Position position,
-			Direction direction)
-	{
-		int nombreDeJetonsAlignes = 0;
-		Position positionSuivante = position.obtenirVoisine(direction);
-		while (true)
-		{
-			if (this.grille.estHorsDeLaGrille(positionSuivante))
-				break;
-			if (this.grille.obtenirCase(positionSuivante) != this.grille
-					.obtenirCase(position))
-				break;
-			nombreDeJetonsAlignes++;
-		}
-		return nombreDeJetonsAlignes;
-	}
-
 }
