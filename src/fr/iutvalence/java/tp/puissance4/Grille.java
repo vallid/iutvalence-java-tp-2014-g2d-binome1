9f25ef22f23bcdadc23edc7d57605e8c82e394df
sebastienjean <baz.jean@gmail.com>
2014-05-03 09:31:31 +0200

    Nettoyage de la classe Grille

--- src/fr/iutvalence/java/tp/puissance4/Grille.java
+++ src/fr/iutvalence/java/tp/puissance4/Grille.java
@@ -1,6 +1,5 @@
 package fr.iutvalence.java.tp.puissance4;
 
-// TODO(Fait) corriger le commentaire
 /**
  * Représente une grille de Puissance4
  * 
@@ -9,25 +8,22 @@ package fr.iutvalence.java.tp.puissance4;
 public class Grille
 {
 	/**
-	 * la longueur par defaut de la grille
+	 * Le nombre de lignes
 	 */
 	public static final int NOMBRE_DE_LIGNES = 6;
 
 	/**
-	 * la largeur par defaut de la grille
+	 * Le nombre de colonnes
 	 */
 	public static final int NOMBRE_DE_COLONNES = 7;
 
 	/**
-	 * tableau de cases pour définir la grille
+	 * Les états des cases de la grille
 	 */
 	public EtatCase[][] etatsDesCases;
 
-	// TODO(fait) écrire un commentaire plus précis (comment est initialisée la
-	// grille ?)
 	/**
-	 * Permet de créer une grille de taille par defaut : 7 colonnes et 6 lignes
-	 * Les cases de la grille sont non occupées
+	 * Création d'une grille vide
 	 */
 	public Grille()
 	{
@@ -35,46 +31,42 @@ public class Grille
 
 		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
 			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-			{
-				this.etatsDesCases[numeroDeLigne][numeroDeColonne] = EtatCase.VIDE;
-			}
-				
+
+				this.etatsDesCases[numeroDeLigne][numeroDeColonne] = EtatCase.VIDE;				
 	}
 
 	/**
-	 * Permet d'obtenir une case à la position indiquée en
-	 * paramètre
-	 * @param position : Position de la case à obtenir
-	 * @return La case
+	 * Obtenir l'état de la case une position donnée
+	 * @param position la position
+	 * @return l'état de la case la position donnée
 	 */
 	public EtatCase obtenirCase(Position position)
 	{
-		// TODO(fait) cas d'erreur ? soit return null, soit une exception
 		if (position.obtenirNumeroDeLigne()<NOMBRE_DE_LIGNES && position.obtenirNumeroDeColonne()<NOMBRE_DE_COLONNES)
 		return this.etatsDesCases[position.obtenirNumeroDeLigne()][position.obtenirNumeroDeColonne()];
-		else return null;
+		
+		return null;
 	}
 	
 
 	/**
-	 * @param position Position de la case que l'on veut modifier
-	 * @param nouvelEtat Nouvel état de la case
+	 * Modifier l'état de la case une position donnée
+	 * @param position la position
+	 * @param nouvelEtat le nouvel état de la case
 	 */
 	public void changerEtatCase(Position position, EtatCase nouvelEtat)
 	{
-		// TODO (Fait) à completer
+		// TODO traiter le cas d'erreur (débordement de grille)
 		this.etatsDesCases[position.obtenirNumeroDeLigne()][position.obtenirNumeroDeColonne()]=nouvelEtat;
 	}
-	/**
-	 * Permet de représenter le puissance 4 en ASCII ART
-	 * 
-	 * @return Représentation ASCII du Puissance 4
-	 */
 
+	/**
+	 * Obtenir une représentation en ASCII-art
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString()
 	{
 		String puissance4EnAscii = "";
-		Position position = new Position(0,0);
 		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
 		{
 			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
@@ -84,10 +76,16 @@ public class Grille
 		return puissance4EnAscii;
 	}
 
-	public boolean estHorsDeLaGrille(Position positionSuivante)
+	/**
+	 * Savoir si une position est hors de la grille
+	 * @param position la position
+	 * @return <tt>true</tt> si la position est hors de la grille
+	 */
+	public boolean estHorsDeLaGrille(Position position)
 	{
-		if (positionSuivante.obtenirNumeroDeLigne() >=0 && positionSuivante.obtenirNumeroDeLigne()< NOMBRE_DE_LIGNES)
-			if (positionSuivante.obtenirNumeroDeColonne()>=0 && positionSuivante.obtenirNumeroDeColonne()< NOMBRE_DE_COLONNES)
+		// TODO retourner le test, ce serait plus clair
+		if (position.obtenirNumeroDeLigne() >=0 && position.obtenirNumeroDeLigne()< NOMBRE_DE_LIGNES)
+			if (position.obtenirNumeroDeColonne()>=0 && position.obtenirNumeroDeColonne()< NOMBRE_DE_COLONNES)
 				return true;
 		return false;
 	}
