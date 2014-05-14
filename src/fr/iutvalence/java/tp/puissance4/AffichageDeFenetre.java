1a30fe2cec5a96338071dd254f57ccbb9c774381
vallid <dylan.valli@iut-valence.fr>
2014-05-14 16:34:13 +0200

    Ajout de la grille
    Affichage d'une grille

--- src/fr/iutvalence/java/tp/puissance4/AffichageDeFenetre.java
+++ src/fr/iutvalence/java/tp/puissance4/AffichageDeFenetre.java
@@ -1,10 +1,13 @@
 package fr.iutvalence.java.tp.puissance4;
 
-
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
+import java.awt.event.ActionListener;
 
 
-public class AffichageDeFenetre implements Runnable
+public class AffichageDeFenetre implements Runnable 
 {
 
 /* (non-Javadoc)
@@ -17,8 +20,21 @@ public void run()
 	
 	fenetre.setTitle("Puissance 4");
 	fenetre.setSize(600, 600);
+	
+	JPanel panel = new JPanel();
+	
+	panel.setLayout(new  GridLayout(7,6));
+	
+	for (int numeroDeBouton = 0; numeroDeBouton<42; numeroDeBouton++){
+		JButton bouton = new JButton("");
+		bouton.setFocusable(false);
+		panel.add(bouton);
+		}
+	fenetre.getContentPane().add(panel);	
 	fenetre.setVisible(true);
+	
 		
 
 }
 }
+
--- src/fr/iutvalence/java/tp/puissance4/AffichageGraphique.java
+++ src/fr/iutvalence/java/tp/puissance4/AffichageGraphique.java
@@ -9,7 +9,7 @@ public class AffichageGraphique
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
 		SwingUtilities.invokeLater(new AffichageDeFenetre());
 	}
 
